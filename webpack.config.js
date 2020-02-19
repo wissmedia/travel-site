0b54671400e54ab7bfb06375a9536590ab35a4cb
Dwis <wissmedia@gmail.com>
2020-02-19 15:34:53 +0800

    add webserver

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,13 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     devServer: {
+        before: function(app, server){
+            server._watch('./app/**/*.html')
+        },
         contentBase: path.join(__dirname, 'app'),
         hot: true,
-        port: 3000
+        port: 3000,
+        host: '0.0.0.0'
     },
     mode: 'development',
     module: {
