c1ea693b48ce1bea2c8d115f8e42a40526bbaa81
Dwis <wissmedia@gmail.com>
2020-02-26 18:04:27 +0800

    add final change

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,7 +4,6 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
-
 new MobileMenu();
 new StickyHeader();
 let modal;
