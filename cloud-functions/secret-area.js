54747921bc80c37c42373bcf27c1d609af1c6049
Dwis <wissmedia@gmail.com>
2020-02-27 14:01:48 +0800

    add cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,24 @@
 exports.handler = function(event, context, callback){
-    callback(null,{
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+    const secretContent = `
+        <h3>Welcome to secret area</h3>
+        <p>nice right? <strong>of course</strong><p/>
+    `
+    let body;
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null,{
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null,{
+            statusCode: 401
+        })
+    }
 }
